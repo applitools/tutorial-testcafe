98332c67083cbd41a8dac879aae656354a78325a
Valandi <valandi.allulis@gmail.com>
2025-05-19 14:13:28 -0400

    Update Testcafe demo

--- tests/play.testcafe.js
+++ tests/play.testcafe.js
@@ -1,25 +1,28 @@
 // next line is optional, just for IDEs autocomplete :
 /// <reference types="@applitools/eyes-testcafe" />
 
-import Eyes from '@applitools/eyes-testcafe';
+import { Eyes, ConsoleLogHandler, ClassicRunner, VisualGridRunner } from "@applitools/eyes-testcafe/dist/index";
+import { Selector } from 'testcafe';
 
 // Initialize the eyes
-const eyes = new Eyes();
+const runner = new VisualGridRunner();
+const eyes = new Eyes(runner);
 
 // Set page used in the test
-fixture`Testcafe Demo App`.page`https://demo.applitools.com`
+fixture`Testcafe Demo App`.page`https://sandbox.applitools.com/bank`
     // Call Close on eyes to let the server know it should display the results
     .afterEach(async () => eyes.close())
     .after(async () => {
         // Wait and collect all test results
         // we pass false to this method to suppress the exception that is thrown if we
-        // find visual differences
+        // find visual differences  
         let allTestResults = await eyes.waitForResults(false)
         // Print results
         console.log(allTestResults)
     });
 
 test('ultraFastTest', async t => {
+    await eyes.setLogHandler(new ConsoleLogHandler(true));
     // Call Open on eyes to initialize a test session
     await eyes.open({
         t, // pass testcafe contorller
@@ -34,6 +37,10 @@ test('ultraFastTest', async t => {
         target: 'window',
         fully: true
     });
+
+    await t
+        .typeText(Selector("#username"), "Chris")
+        .typeText(Selector("#password"), "CorrectHorseBatteryStaple")
 
     // This will create a test with two test steps.
     await t.click('#log-in')
